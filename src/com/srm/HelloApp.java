cfc1610b0d2c012e7cd608ba6d07a4a98ee1873b
aakshaya302007 <aakshayavengatesan@gmail.com>
2026-04-01 20:34:15 +0530

    Add HelloApp class to greet users

---
+++ src/com/srm/HelloApp.java
@@ -0,0 +1,10 @@
+package com.srm.helloapp;
+public class HelloApp {
+    public static void main(String[] args) {
+        String name = "Aakshaya";
+        if (args.length > 0) {
+            name = args[0];
+        }
+        System.out.println("Hello, " + name + "!");
+    }
+}
