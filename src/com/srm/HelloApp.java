a970650f188f193cccd7a0dfd63bfdf77f085e1f
aakshaya302007 <aakshayavengatesan@gmail.com>
2026-04-06 10:14:19 +0530

    UC5

---
+++ src/com/srm/HelloApp.java
@@ -0,0 +1,24 @@
+package com.srm.helloapp;
+
+public class Helloapps {
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        StringBuilder nameBuilder = new StringBuilder();
+        boolean first = true;
+
+        for (String name : args) {
+            if (!first) {
+                nameBuilder.append(", ");
+            }
+            nameBuilder.append(name);
+            first = false;
+        }
+
+        System.out.println("Hello, " + nameBuilder + "!");
+    }
+}
