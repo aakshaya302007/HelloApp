c9c5e1369270a12a2f672eabe3a07f5cc2f4a049
aakshaya302007 <aakshayavengatesan@gmail.com>
2026-04-10 08:41:09 +0530

    UC7
    Updated main method to greet with provided arguments.

--- src/com/srm/helloapp/HelloApp.java
+++ src/com/srm/helloapp/HelloApp.java
@@ -1,7 +1,13 @@
 package com.srm.helloapp;
-
 public class HelloApp {
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+	    public static void main(String[] args) {
+
+	        if (args.length == 0) {
+	            System.out.println("Hello, World!");
+	        } else {
+	            
+	            String result = String.join(", ", args);
+	            System.out.println("Hello, " + result + "!");
+	        }
+	    }
 	}
-}
